abeedc14ac0aac134cf0729c8afd8ddf60413b5d
Nathan Dubord <nathandubord@gmail.com>
2019-01-25 19:11:16 -0500

    Add base game manager

---
+++ GGJ2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public List<GameObject> playerSpawnPoints = new List<GameObject>();
+	public List<GameObject> itemSpawnPoints = new List<GameObject>();
+
+    // public PlayerController[] playerPrefabs;
+
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		time = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+	}
+}
