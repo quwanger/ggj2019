b271b06ff64d4a7f88a1ec443168d7f6c02f9b2a
Rufino Ansara <rufinoansara@gmail.com>
2019-01-25 19:13:40 -0500

    Basic arm - point a to b animation
    Added basic point a to b animation using a line renderer

---
+++ GGJ2019/Assets/Scripts/RobotArm.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotArm : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+    private float counter;
+    private float dist;
+
+    public Transform origin;
+    public Transform destination;
+
+    public float drawSpeed = 6f;
+
+	// Use this for initialization
+	void Start () {
+
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, origin.position);
+        lineRenderer.SetWidth(0.45f, 0.45f);
+
+        dist = Vector3.Distance(origin.position, destination.position);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(counter < dist)
+        {
+            counter += 0.1f / drawSpeed;
+
+            float x = Mathf.Lerp(0, dist, counter);
+
+            Vector3 pointA = origin.position;
+            Vector3 pointB = destination.position;
+
+            //get the unit vector in the desired direction, multiply by the desired length and add the starting point.
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+            lineRenderer.SetPosition(1, pointAlongLine);
+        }
+		
+	}
+}
