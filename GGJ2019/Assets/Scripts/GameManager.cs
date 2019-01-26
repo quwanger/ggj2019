64aa97e984098e2877d04c90490e67cc2f46d195
Nathan Dubord <nathandubord@gmail.com>
2019-01-25 19:42:13 -0500

    basic classes for base game functionality

--- GGJ2019/Assets/Scripts/GameManager.cs
+++ GGJ2019/Assets/Scripts/GameManager.cs
@@ -7,16 +7,14 @@ public class GameManager : MonoBehaviour {
 	public List<GameObject> playerSpawnPoints = new List<GameObject>();
 	public List<GameObject> itemSpawnPoints = new List<GameObject>();
 
-    // public PlayerController[] playerPrefabs;
+    public PlayerController[] playerPrefabs;
 
 	public float time;
 
-	// Use this for initialization
 	void Start () {
 		time = 0.0f;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
 	}
