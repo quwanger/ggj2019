6064cb9ef1a7bc5d8654ee91bd356cb9bf53d9e0
Tyson <tysonjfmoyes@gmail.com>
2019-01-25 22:34:28 -0500

    Starting controller setup

--- GGJ2019/Assets/Scripts/PlayerController.cs
+++ GGJ2019/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-	void Start () {
+    public XboxController controller;
+    public int controllerId;
+    public int speed = 5;
+
+	void Start ()
+    {
+        SetupControls();
 	}
 	
-	void Update () {
-	}
+	void Update ()
+    {
+        ControllerInput();
+    }
+
+    private void SetupControls()
+    {
+        controller = new XboxController(controllerId);
+    }
+
+    private void ControllerInput()
+    {
+        transform.RotateAround(this.transform.position, Vector3.back, speed * Input.GetAxis(controller.joyLeftVert));
+
+        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //transform.rotation = Quaternion.LookRotation(this.transform.position - mousePosition, Vector3.forward);
+    }
 }
