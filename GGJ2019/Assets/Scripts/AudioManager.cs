edb4febe760ebb535bb83a63af825c1d22e6cf19
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 14:51:21 -0500

    Added audio manager script

---
+++ GGJ2019/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioClip[] pushes;
+    public AudioClip[] pulls;
+    public AudioClip[] explosions;
+    public AudioClip[] cheers;
+    public AudioClip[] boos;
+
+    public AudioSource[] sources;
+
+    AudioSource sfx;
+    AudioSource soundtrack;
+
+	// Use this for initialization
+	void Start () {
+        sources = GetComponents<AudioSource>();
+        //sfx = sources[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaySound(string code)
+    {
+        switch (code)
+        {
+            case "pushes":
+                sfx.PlayOneShot(pushes[Random.Range(0, pushes.Length)]);
+                break;
+            case "pulls":
+                sfx.PlayOneShot(pulls[Random.Range(0, pulls.Length)]);
+                break;
+            case "explosions":
+                sfx.PlayOneShot(explosions[Random.Range(0, explosions.Length)]);
+                break;
+            case "cheers":
+                sfx.PlayOneShot(cheers[Random.Range(0, cheers.Length)]);
+                break;
+            case "boos":
+                sfx.PlayOneShot(boos[Random.Range(0, boos.Length)]);
+                break;
+        }
+    }
+}
