e92202b11f426a399811e3a36ef1232496249afa
Rufino Ansara <rufinoansara@gmail.com>
2019-01-27 11:32:55 -0500

    Fix error on ScopeRender

--- GGJ2019/Assets/ScopeRender.cs
+++ GGJ2019/Assets/ScopeRender.cs
@@ -8,9 +8,7 @@ public class ScopeRender : MonoBehaviour {
 
     void Awake()
     {
-        line = GetComponent<LineRenderer>();
-        Material myMaterial = line.GetComponent<Material>();
-        myMaterial.mainTextureScale = new Vector2(100,100);
+       
     }
 
     void Update()
@@ -18,20 +16,4 @@ public class ScopeRender : MonoBehaviour {
         
     }
 
-    private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
-    {
-        Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, false);
-        float incX = (1.0f / (float)targetWidth);
-        float incY = (1.0f / (float)targetHeight);
-        for (int i = 0; i < result.height; ++i)
-        {
-            for (int j = 0; j < result.width; ++j)
-            {
-                Color newColor = source.GetPixelBilinear((float)j / (float)result.width, (float)i / (float)result.height);
-                result.SetPixel(j, i, newColor);
-            }
-        }
-        result.Apply();
-        return result;
-    }
 }
